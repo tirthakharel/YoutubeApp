6c33784b18764cc470b20b3e48e267ad3014325f
Tirtha Kharel <tkharel21@gmail.com>
2020-07-19 15:05:53 -0500

    initiali XCode project creation

---
+++ youtube-details/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-details
+//
+//  Created by Tirtha Kharel on 7/19/20.
+//  Copyright © 2020 Tirtha Kharel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
